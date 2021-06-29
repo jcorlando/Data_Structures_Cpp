25872a7e2eb7af5b4d1b43b11d1eafbf92c5c884
Juan <jcorlandomc@yahoo.com>
2021-06-29 15:42:26 -0700

    2021-06-29 3:42 PM

--- circularbuffer/mycircularbuffer.hpp
+++ circularbuffer/mycircularbuffer.hpp
@@ -1,24 +1,39 @@
 #ifndef MYCIRCULARBUFFER_H
 #define MYCIRCULARBUFFER_H
 #include <iostream>
+#include <vector>
 
 template<class T>
 class mycircularbuffer
 {
 private:
-    /* data */
+    std::vector<T> vectr;
 public:
-    mycircularbuffer(/* args */);
-    {
-        
-    }
+    mycircularbuffer();
+    int vectr_size() const;
+    void insert_end(const T &value);
+    T get_at(const int &value) const;
 };
 
+template<class T>
+mycircularbuffer<T>::mycircularbuffer() { ; }
 
 template<class T>
-mycircularbuffer<T>::mycircularbuffer(/* args */)
+T mycircularbuffer<T>::get_at(const int &value) const
 {
+    return vectr.at(value);
 }
 
+template<class T>
+void mycircularbuffer<T>::insert_end(const T &value)
+{
+    vectr.push_back(value);
+}
+
+template<class T>
+int mycircularbuffer<T>::vectr_size() const
+{
+    return vectr.size();
+}
 
 #endif // MYCIRCULARBUFFER_H
