76e173e1e73401f72e168a3d3bb7a6a062eca1f1
Juan <jcorlandomc@yahoo.com>
2021-06-28 20:37:56 -0700

    Jun-28-2021 8:37 PM

--- array/main.cpp
+++ array/main.cpp
@@ -3,6 +3,9 @@
 // Driver code
 int main()
 {
+    //////////////////////////////////////////////////////////////////////////////////
+    ///////// This code is my own implemantation using modern C++ techniques /////////
+    //////////////////////////////////////////////////////////////////////////////////
     myvector<int> v;
     myvector<char> v1;
     v.push(10);
@@ -10,43 +13,55 @@ int main()
     v.push(30);
     v.push(40);
     v.push(50);
-    // v1.push(71);
-    // v1.push(72);
-    // v1.push(73);
-    // v1.push(74);
+    v1.push(71);
+    v1.push(72);
+    v1.push(73);
+    v1.push(74);
  
-    // std::cout << "Vector size : " << v.size() << std::endl;
-    // std::cout << "Vector capacity : " << v.getcapacity() << std::endl;
+    std::cout << "Vector size : " << v.size() << std::endl;
+    std::cout << "Vector capacity : " << v.getcapacity() << std::endl;
  
-    // std::cout << "Vector elements : ";
-    // v.print();
+    std::cout << "Vector elements : ";
+    v.print();
  
-    // v.push(100, 1);
+    v.push(100, 1);
  
-    // std::cout << "\nAfter updating 1st index" << std::endl;
+    std::cout << "\nAfter updating 1st index" << std::endl;
  
-    // std::cout << "Vector elements of type int : " << std::endl;
-    // v.print();
-    // // This was possible because we used templates
-    // std::cout << "Vector elements of type char : " << std::endl;
-    // v1.print();
-    // std::cout << "Element at 1st index of type int: " << v.get(1) << std::endl;
-    // std::cout << "Element at 1st index of type char: " << v1.get(1) << std::endl;
+    std::cout << "Vector elements of type int : " << std::endl;
+    v.print();
+    // This was possible because we used templates
+    std::cout << "Vector elements of type char : " << std::endl;
+    v1.print();
+    std::cout << "Element at 1st index of type int: " << v.get(1) << std::endl;
+    std::cout << "Element at 1st index of type char: " << v1.get(1) << std::endl;
  
-    // v.pop();
-    // v1.pop();
+    v.pop();
+    v1.pop();
  
-    // std::cout << "\nAfter deleting last element" << std::endl;
+    std::cout << "\nAfter deleting last element" << std::endl;
  
-    // std::cout << "Vector size of type int: " << v.size() << std::endl;
-    // std::cout << "Vector size of type char: " << v1.size() << std::endl;
-    // std::cout << "Vector capacity of type int : " << v.getcapacity() << std::endl;
-    // std::cout << "Vector capacity of type char : " << v1.getcapacity() << std::endl;
+    std::cout << "Vector size of type int: " << v.size() << std::endl;
+    std::cout << "Vector size of type char: " << v1.size() << std::endl;
+    std::cout << "Vector capacity of type int : " << v.getcapacity() << std::endl;
+    std::cout << "Vector capacity of type char : " << v1.getcapacity() << std::endl;
  
-    // std::cout << "Vector elements of type int: ";
-    // v.print();
-    // std::cout << "Vector elements of type char: ";
-    // v1.print();
+    std::cout << "Vector elements of type int: ";
+    v.print();
+    std::cout << "Vector elements of type char: ";
+    v1.print();
+    //////////////////////////////////////////////////////////////////////////////////
+    ///////// This code is my own implemantation using modern C++ techniques /////////
+    //////////////////////////////////////////////////////////////////////////////////
+
+
+    //////////////////////////////////////////////////////////////////////////////////
+    ///////////// This code is the same as above using built-in C++ STL //////////////
+    //////////////////////////////////////////////////////////////////////////////////
+    
+    //////////////////////////////////////////////////////////////////////////////////
+    ///////////// This code is the same as above using built-in C++ STL //////////////
+    //////////////////////////////////////////////////////////////////////////////////
 
     return 0;
 }
--- array/myvector.cpp
+++ array/myvector.cpp
@@ -3,7 +3,6 @@
 #include <memory>
 #include <iostream>
 
-
 template <class T>
 class myvector
 {
@@ -24,76 +23,70 @@ class myvector
             capacity = 1;
             current = 0;
         }
-        // Function to add an element at the last
+
+        // Function to add an element at the last at the end
         void push(T data)
         {
-            std::cout << "\"capacity\" Variable == " << capacity << std::endl;
-            std::cout << "\"current\"  Variable == " << current << std::endl;
             // if the number of elements is equal to the
             // capacity, that means we don't have space to
             // accommodate more elements. We need to double the
             // capacity
             if (current == capacity)
             {
-                std::shared_ptr<T[]> temp = std::make_shared<T[]>(2 * capacity);
+                int new_capacity = 2 * capacity;
+                std::unique_ptr<T[]> temp = std::make_unique<T[]>(new_capacity);
                 // // copying old array elements to new array
-                // for (int i = 0; i < capacity; i++)
-                // {
-                //     temp[i] = arr[i];
-                // }
-                // // deleting previous array
-                // delete[] arr;
-                // capacity *= 2;
-                // arr = temp;
-
-
-
-
+                for (int i = 0; i < capacity; i++)
+                    { temp[i] = arr[i]; }
+                // reset previous array using unique pointers
+                arr.reset();
+                arr = std::make_unique<T[]>(new_capacity);
+                for (int i = 0; i < new_capacity; i++)
+                    { arr[i] = temp[i]; }
+                capacity = new_capacity;
             }
-    
-            // Inserting data
+            // Insert data into the last element
             arr[current] = data;
             current++;
         }
     
-        // // function to add element at any index
-        // void push(int data, int index)
-        // {
+        // function to add element at any index
+        void push(int data, int index)
+        {
+            // if index is equal to capacity then this
+            // function is same as push defined above
+            if (index == capacity)
+                push(data);
+            else
+                arr[index] = data;
+        }
     
-        //     // if index is equal to capacity then this
-        //     // function is same as push defined above
-        //     if (index == capacity)
-        //         push(data);
-        //     else
-        //         arr[index] = data;
-        // }
+        // function to extract element at any index
+        T get(int index)
+        {
+            // if index is within the range
+            if (index < current)
+                return arr[index];
+        }
+
+        // function to delete last element
+        void pop() { current--; }
     
-        // // function to extract element at any index
-        // T get(int index)
-        // {
-        //     // if index is within the range
-        //     if (index < current)
-        //         return arr[index];
-        // }
+        // function to get size of the vector
+        int size() { return current; }
     
-        // // function to delete last element
-        // void pop() { current--; }
+        // function to get capacity of the vector
+        int getcapacity() { return capacity; }
     
-        // // function to get size of the vector
-        // int size() { return current; }
-    
-        // // function to get capacity of the vector
-        // int getcapacity() { return capacity; }
-    
-        // // function to print array elements
-        // void print()
-        // {
-        //     for (int i = 0; i < current; i++)
-        //     {
-        //         std::cout << arr[i] << " ";
-        //     }
-        //     std::cout << std::endl;
-        // }
+        // function to print array elements
+        void print()
+        {
+            for (int i = 0; i < current; i++)
+            {
+                std::cout << arr[i] << " ";
+            }
+            std::cout << std::endl;
+        }
 };
 
 #endif
