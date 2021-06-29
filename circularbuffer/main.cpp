972c94653652ee337118a53ec425b5c657e61d63
Juan <jcorlandomc@yahoo.com>
2021-06-29 12:02:48 -0700

    Jun-29-2021 12:02 PM

---
+++ circularbuffer/main.cpp
@@ -0,0 +1,11 @@
+#include "mycircularbuffer.hpp"
+
+// My implementation of a circular
+// buffer. It will most likely be
+// used for DSP related algorithms
+
+int main()
+{
+    
+    return 0;
+}
---
+++ circularbuffer/mycircularbuffer.hpp
@@ -0,0 +1,24 @@
+#ifndef MYCIRCULARBUFFER_H
+#define MYCIRCULARBUFFER_H
+#include <iostream>
+
+template<class T>
+class mycircularbuffer
+{
+private:
+    /* data */
+public:
+    mycircularbuffer(/* args */);
+    {
+        
+    }
+};
+
+
+template<class T>
+mycircularbuffer<T>::mycircularbuffer(/* args */)
+{
+}
+
+
+#endif // MYCIRCULARBUFFER_H
