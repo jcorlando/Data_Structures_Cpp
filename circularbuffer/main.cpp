910eef4dff743042f1116d18437199200af47882
Juan <jcorlandomc@yahoo.com>
2021-10-10 22:19:48 -0700

    Oct-10-2021 10:19 PM

--- circularbuffer/main.cpp
+++ circularbuffer/main.cpp
@@ -1,5 +1,7 @@
 #include "mycircularbuffer.hpp"
 
+#include <iostream>
+
 // My implementation of a circular
 // buffer. It will most likely be
 // used for DSP related algorithms
@@ -7,10 +9,12 @@
 int main()
 {
     // Create Circular buffer object
-    mycircularbuffer<double, 900> buff_1;
+    mycircularbuffer<double, 5> buff_1;
     std::cout << "The Array size is : " << buff_1.array_size() << std::endl;
 
-    
+    std::cout << "The Front is at : " << buff_1.get_front() << std::endl;
+
+    std::cout << "The Back is at : " << buff_1.get_back() << std::endl;
 
     std::cout << "Program Done!" << std::endl;
     return 0;
--- circularbuffer/mycircularbuffer.hpp
+++ circularbuffer/mycircularbuffer.hpp
@@ -1,20 +1,33 @@
 #ifndef MYCIRCULARBUFFER_H
 #define MYCIRCULARBUFFER_H
-#include <iostream>
+
 #include <array>
-#include <vector>
+#include <memory>
 
 template<class T, unsigned int size>
 class mycircularbuffer
 {
 private:
     std::array<T, size> array;
+    unsigned int front;
+    unsigned int current_length;
 public:
     // Constructor
     mycircularbuffer();
 
+    
+
+
+    // Get the Front Pointer
+    unsigned int get_front() const;
+    
+    // Get the Back Pointer
+    unsigned int get_back() const;
+
+    // Get the full array size
     int array_size() const;
 
+    // get the value at the specified index
     T get_at(const int &value) const;
 };
 
@@ -22,7 +35,11 @@ public:
 
 // Constructor
 template<class T, unsigned int size>
-mycircularbuffer<T, size>::mycircularbuffer() { ; }
+mycircularbuffer<T, size>::mycircularbuffer()
+{
+    front = 0;
+    current_length = 0;
+}
 
 
 template<class T, unsigned int size>
@@ -38,5 +55,14 @@ T mycircularbuffer<T, size>::get_at(const int &value) const
 
 template<class T, unsigned int size>
 int mycircularbuffer<T, size>::array_size() const   { return array.size(); }
+
+
+template<class T, unsigned int size>
+unsigned int mycircularbuffer<T, size>::get_front() const   { return this->front; }
+
+
+template<class T, unsigned int size>
+unsigned int mycircularbuffer<T, size>::get_back() const   { return this->front; }
+
 
 #endif // MYCIRCULARBUFFER_H
