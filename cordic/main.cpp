e4f9c4b5d77fada6ff31aad34c8a2b103ab21fa2
Juan <jcorlandomc@yahoo.com>
2021-10-16 16:41:06 -0700

    Oct-16-2021 4:41 PM

--- cordic/main.cpp
+++ cordic/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <cmath>
+#include <array>
 #include <unistd.h>
 #include "mycordic.hpp"
 
-
+#define CORDIC_PRECISION 10
 
 int main()
 {
     // <---------------------------->
     
-    uint8_t i = 0; // iteration value
+    uint8_t iter = 0; // iteration value
 
-    
+    // LUT that holds inverse tangents of powers 2^-1
+    std::array<double, CORDIC_PRECISION> array;
+
+    for(int i = 0; i < 10; i++) std::cout << atan(std::pow(2, -i)) << std::endl;
+
 
     return 0;
 }
