4f07823e8c04312ad14c73cf424d20a8368eb713
Juan <jcorlandomc@yahoo.com>
2021-10-16 13:39:04 -0700

    Oct-16-2021 1:39 PM

--- cordic/main.cpp
+++ cordic/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <cmath>
 #include "mycordic.hpp"
-
 
 int main()
 {
-
+    // <---------------------------->
+    std::cout << "Pi = " << M_PI << std::endl;
+    std::cout << "Sin of 0 = " << sin(0.0) << std::endl;
+    
 
     // Program Done!
     std::cout << "Program Done!" << std::endl;
