a6ae93e8b1cb9d1fa661b2c7175fb84bcdef0a54
Juan <jcorlandomc@yahoo.com>
2021-10-17 14:42:53 -0700

    Oct-17-2021 2:42 PM

--- cordic/readout.py
+++ cordic/readout.py
@@ -1,31 +1,35 @@
-import wave
 import numpy as np
-import sys
-import math
-import struct
 import matplotlib.pyplot as plt
-from scipy import signal
-from scipy.signal import lfilter, butter, kaiserord, firwin, freqz
-from scipy.io import wavfile
-
 
 with open('output.dat') as f:
     lines = f.read().splitlines()
-
 
 cosine = []
 sine = []
 
 for each in range(len(lines)):
-    cosine.append(lines[each].split(',')[0])
-    sine.append(lines[each].split(',')[1])
+    cosine.append(float(lines[each].split(',')[0]))
+    sine.append(float(lines[each].split(',')[1]))
 
-# for each in range(len(cosine)):
-#     print(cosine[each])
+#------------------Define and set x-axis------------------------
+dt = np.linspace(0, 360, 1081)
+#------------------Define and set x-axis------------------------
 
-# for each in range(len(sine)):
-#     print(sine[each])
+#---------------Plot Cosine---------------
+plt.figure(1)
+plt.title("Cosine")
+plt.plot(dt, cosine)
+plt.xlabel("Degrees")
+#---------------Plot Cosine---------------
 
+#---------------Plot Sine---------------
+plt.figure(2)
+plt.title("Sine")
+plt.plot(dt, sine)
+plt.xlabel("Degrees")
+#---------------Plot Sine---------------
 
-
+#-------Show all Plots and Figures-------
+plt.show()
+#-------Show all Plots and Figures-------
 
