8b5bc9afac2dc86ee03d0f8f71b1707a1cd36557
Juan <jcorlandomc@yahoo.com>
2021-10-16 19:24:45 -0700

    Oct-16-2021 7:24 PM

--- cordic/main.cpp
+++ cordic/main.cpp
@@ -4,28 +4,47 @@
 #include <unistd.h>
 #include "mycordic.hpp"
 
-#define LUT_PRECISION 10
+// CORDIC approximation algorithm to be tested
+// against the  built in C++ library algorithm
+
+// Change this to create a more precise algortihm
+#define LUT_PRECISION 20
+
+// angle to calculate SIN or COS of
+double angle = 30.0;
 
 // LUT that holds inverse tangents of powers 2^-1
 std::array<double, LUT_PRECISION> lut;
 
+// scaling factor for x and y coordinates
 const double K = 0.6072529350088812561694;
+
 
 int main()
 {
-    // <---------------------------->
-    
-    uint8_t i = 0; // <--- CORDIC iteration value
-
-    // Populate array with values
-    for(int j = 0; j < LUT_PRECISION; j++) lut[j] = atan(std::pow(2, -j));
+    // Populate array with values !!!! Multiply by *(180/M_PI) to convert to degrees
+    for(int i = 0; i < LUT_PRECISION; i++) lut[i] = atan(std::pow(2, -i))*(180/M_PI);
 
     // print values to ensure correctness
     // for(auto each : lut) std::cout << each << std::endl;
     
     
-    // 
-    // 
+    for(auto each : lut)
+    {
+        if(angle >= 0)
+        {
+            std::cout << angle << " - " << each << " = ";
+            angle = angle - each;
+            std::cout << angle << std::endl;
+        }
+        else
+        {
+            std::cout << angle << " + " << each << " = ";
+            angle = angle + each;
+            std::cout << angle << std::endl;
+        }
+    }
+    
 
     
 
