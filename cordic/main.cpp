83592b2969a92f37da524764a05981ecb5c19569
Juan <jcorlandomc@yahoo.com>
2021-10-16 19:57:52 -0700

    Oct-16-2021 7:57 PM

--- cordic/main.cpp
+++ cordic/main.cpp
@@ -7,43 +7,17 @@
 // CORDIC approximation algorithm to be tested
 // against the  built in C++ library algorithm
 
-// Change this to create a more precise algortihm
-#define LUT_PRECISION 20
-
-// angle to calculate SIN or COS of
-double angle = 30.0;
-
-// LUT that holds inverse tangents of powers 2^-1
-std::array<double, LUT_PRECISION> lut;
-
-// scaling factor for x and y coordinates
-const double K = 0.6072529350088812561694;
 
 
 int main()
 {
-    // Populate array with values !!!! Multiply by *(180/M_PI) to convert to degrees
-    for(int i = 0; i < LUT_PRECISION; i++) lut[i] = atan(std::pow(2, -i))*(180/M_PI);
+    mycordic::populate_lut();
 
     // print values to ensure correctness
-    // for(auto each : lut) std::cout << each << std::endl;
+    for(auto each : mycordic::lut) std::cout << each << std::endl;
     
     
-    for(auto each : lut)
-    {
-        if(angle >= 0)
-        {
-            std::cout << angle << " - " << each << " = ";
-            angle = angle - each;
-            std::cout << angle << std::endl;
-        }
-        else
-        {
-            std::cout << angle << " + " << each << " = ";
-            angle = angle + each;
-            std::cout << angle << std::endl;
-        }
-    }
+    // mycordic::calc_cordic();
     
 
     
--- cordic/mycordic.hpp
+++ cordic/mycordic.hpp
@@ -2,7 +2,43 @@
 #define MYCORDIC_H
 
 
+namespace mycordic
+{
+    // Change this to create a more precise algortihm
+    #define LUT_PRECISION 20
+
+    // angle to calculate SIN or COS of
+    double angle = 30.0;
+
+    // LUT that holds inverse tangents of powers 2^-1
+    std::array<double, LUT_PRECISION> lut;
+
+    // Populate array with values !!!! Multiply by *(180/M_PI) to convert to degrees
+    void populate_lut() { for(int i = 0; i < LUT_PRECISION; i++) lut[i] = atan(std::pow(2, -i))*(180/M_PI); }
+
+    // scaling factor for x and y coordinates
+    const double K = 0.6072529350088812561694;
 
 
+    void calc_cordic()
+    {
+        for(auto each : lut)
+        {
+            if(angle >= 0)
+            {
+                angle = angle - each;
+            }
+            else
+            {
+                angle = angle + each;
+            }
+        }
+    }
+
+
+
+
+
+}
 
 #endif // MYCORDIC_H
