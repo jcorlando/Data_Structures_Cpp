490fdd69868adebd0e816e941041f95aac332175
Juan <jcorlandomc@yahoo.com>
2021-10-16 22:18:21 -0700

    Oct-16-2021 10:18 PM

--- cordic/mycordic.hpp
+++ cordic/mycordic.hpp
@@ -15,7 +15,7 @@ namespace mycordic
     double x;
     double y;
     double d;
-    unsigned int counter;
+    double counter;
 
     // LUT that holds inverse tangents of powers 2^-1
     std::array<double, LUT_PRECISION> lut;
@@ -29,6 +29,11 @@ namespace mycordic
     {
         double x_temp = x;
         double y_temp = y;
+        std::cout << "  x   is  : " << x << std::endl;
+        std::cout << "  y   is  : " << y << std::endl;
+        std::cout << "x_temp is : " << x_temp << std::endl;
+        std::cout << "y_temp is : " << y_temp << std::endl;
+        std::cout << "  d   is  : " << d << std::endl;
         x = K*( x_temp - ( (d)*(y_temp)*(std::pow(2, -counter)) ) );
         y = K*( y_temp + ( (d)*(x_temp)*(std::pow(2, -counter)) ) );
     }
@@ -38,10 +43,11 @@ namespace mycordic
     void calc_cordic(double angle)
     {
         // Initialize x and y values
-        x = 1.0;  y = 0.0;  counter = 0;
+        counter = 0;  x = 1.0;  y = 0.0;
         
         for(auto each : lut)
         {
+            std::cout << std::endl << "iteration : " << counter + 1 << std::endl << std::endl;
             if(angle >= 0)
             {
                 d = 1.0;
@@ -52,10 +58,9 @@ namespace mycordic
                 d = -1.0;
                 angle = angle + each;
             }
-
+            
             x_y_calculation();
             counter++;
-
         }
 
     }
