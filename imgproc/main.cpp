032e91f83943d3ba8317059aad18fc9d7c585ba9
Juan <jcorlandomc@yahoo.com>
2021-10-17 21:43:35 -0700

    Oct-17-2021 9:43 PM

--- imgproc/main.cpp
+++ imgproc/main.cpp
@@ -1,10 +1,32 @@
+#include <opencv2/opencv.hpp>
 #include <iostream>
 #include "imageprocessing.hpp"
+
+using namespace cv;
 
 
 int main()
 {
-    
+    // Read the image file
+    Mat image = imread("./Hello.jpg");
+
+    // Check for failure
+    if (image.empty())
+    {
+        cout << "Could not open or find the image" << endl;
+        cin.get(); //wait for any key press
+        return -1;
+    }
+
+    String windowName = "The Guitar"; //Name of the window
+
+    namedWindow(windowName); // Create a window
+
+    imshow(windowName, image); // Show our image inside the created window.
+
+    waitKey(0); // Wait for any keystroke in the window
+
+    destroyWindow(windowName); //destroy the created window
 
     return 0;
 }
