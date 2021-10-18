9eb89837801ef32edd2380e2a576f4a463b7904d
Juan <jcorlandomc@yahoo.com>
2021-10-17 18:10:12 -0700

    Oct-17-2021 6:10 PM

---
+++ 2dvectors/2dvectors.hpp
@@ -0,0 +1,18 @@
+#ifndef TWODVECTORS_H
+#define TWODVECTORS_H
+
+
+namespace my2dvectors
+{
+    template <class T>
+    class my2dvector
+    {
+        private:
+            ;
+        public:
+            ;
+    };
+
+
+}
+#endif // TWODVECTORS_H
---
+++ 2dvectors/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "2dvectors.hpp"
+
+// CORDIC approximation algorithm to be tested
+// against the  built in C++ library algorithm
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
