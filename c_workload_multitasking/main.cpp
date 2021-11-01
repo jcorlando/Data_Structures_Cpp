b0503ae23b73e81e6be3f338d4f586152e4bf4a0
Juan <jcorlandomc@yahoo.com>
2021-10-31 23:52:50 -0700

    Oct-31-2021 11:52 PM

--- c_workload_multitasking/main.cpp
+++ c_workload_multitasking/main.cpp
@@ -1,15 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <pthread.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
 #include <time.h>
+#include <math.h>
 
-#define N 10000000000   // <- Change this between 100000000 and 10000000000
-#define NUM_THREADS 2   // 
+#define N 100000000   // <- valid values: 100000000, 1000000000 , 10000000000
+#define NUM_TASKS 2   // <- valid values:     2,         4,           8
 
 void* WORKLOAD(long *a)
 {
-  for (long i = 0; i < N; ++i)  (*a)++;
+  for(uint i = 0; i < (uint)log2(NUM_TASKS); i++) pid_t pid = fork();
+  
+
+  printf("pid is : %d\n", getpid());
+  
+
+  // for(long i = 0; i < N; ++i)  (*a)++;
+
+  
   return NULL;
 }
 
@@ -22,14 +33,15 @@ int main()
     // Setup variables
 
 
-    start = clock();
+    // Start timer and call function
+    // start = clock();
     WORKLOAD(&a);
-    end = clock();
-
+    // end = clock();
+    // cpu_time_used = ( (double) (end - start) ) / CLOCKS_PER_SEC;
+    // Start timer and call function
 
     // Print results
-    cpu_time_used = ( (double) (end - start) ) / CLOCKS_PER_SEC;
-    printf("WORKLOAD took %f seconds to execute \nThe value of a is : %ld\n", cpu_time_used, a);
+    // printf("WORKLOAD took %f seconds to execute \nThe value of a is : %ld\n", cpu_time_used, a);
     // Print results
     return 0;
 }
