2d7dcfce3fb45ba1b20ef4243ca854b51a6e4723
Juan <jcorlandomc@yahoo.com>
2021-11-02 22:42:22 -0700

    Nov-02-2021 10:42 PM

--- c_workload_multitasking/main.cpp
+++ c_workload_multitasking/main.cpp
@@ -7,32 +7,12 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+// My Macros
 #define N 100000000   // <- valid values: 100000000, 1000000000 , 10000000000
 #define NUM_TASKS 2   // <- valid values:     2,         4,           8
 
-void WORKLOAD(long &a)
-{
-  // Fork first process and get parent process ID
-  pid_t pid = fork();
-  pid_t ppid = 0;
-  if(pid < 0)  /**/  fprintf(stderr, "Fork Failed\n");
-  if(pid > 0) ppid = getpid();
+#include "workload.hpp"
 
-  // Create the required number of additional processes
-  for(uint i = 2; i < NUM_TASKS; i++)
-  {
-    if(pid == 0) pid = fork();
-    if(pid < 0)  /**/  fprintf(stderr, "Fork Failed\n");
-  }
-
-  std::cout << "pid is : " << getpid() << std::endl;
-  
-
-  // for(long i = 0; i < N; ++i)  (*a)++;
-
-  // Kill all child processes
-  if(pid != ppid) exit(0);
-}
 
 int main()
 {
---
+++ c_workload_multitasking/workload.hpp
@@ -0,0 +1,29 @@
+#ifndef WORKLOAD_H
+#define WORKLOAD_H
+
+void WORKLOAD(long &a)
+{
+  // Fork first process and get parent process ID
+  pid_t pid = fork();
+  pid_t ppid = 0;
+  if(pid < 0)  /**/  fprintf(stderr, "Fork Failed\n");
+  if(pid > 0) ppid = getpid();
+
+  // Create the required number of additional processes
+  for(uint i = 2; i < NUM_TASKS; i++)
+  {
+    if(pid == 0) pid = fork();
+    if(pid < 0)  /**/  fprintf(stderr, "Fork Failed\n");
+  }
+
+
+  // for(long i = 0; i < N; ++i)  (*a)++;
+
+
+  std::cout << "pid is : " << getpid() << std::endl;
+  // Kill all child processes
+  if(pid != ppid) exit(0);
+}
+
+
+#endif // WORKLOAD_H
