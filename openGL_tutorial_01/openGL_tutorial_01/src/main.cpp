b56d938022293f92c7c9d97b24186d539f5ba5c4
Juan Carlos <mrfpgapro@gmail.com>
2022-02-06 19:23:06 -0800

    02/06/2022  -  7:22 PM

--- openGL_tutorial_01/openGL_tutorial_01/src/main.cpp
+++ openGL_tutorial_01/openGL_tutorial_01/src/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 
 int main()
 {
-	std::cout << "Hello Everyone!" << std::endl;
+	std::cout << "Hello OpenGL, GLaD and GLFW!" << std::endl;
+
+	glfwInit();
+
+	// Start OpenGl 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
 	return 0;
 }
 
