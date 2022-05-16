a2ff9a954bbf8670690d3a8cb56eb8202b2ae335
Juan Carlos Orlando <mrfpgapro@gmail.com>
2022-05-16 01:49:50 +0000

    May 15 6:49 PM

---
+++ copy_program/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main()
+{
+	char name[1024] = "Hello_File.txt"; 
+	printf("Enter the name of the source file: ");
+	printf("\nThe name of the source file is  :=  %s\n", name);
+	return 0;
+}
+
+
