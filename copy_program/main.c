27c57db103cfd7be9621ee0515e2592d624bfc4c
Juan Carlos Orlando <mrfpgapro@gmail.com>
2022-05-16 02:08:07 +0000

    May 15 7:07 PM

--- copy_program/main.c
+++ copy_program/main.c
@@ -3,9 +3,12 @@
 
 int main()
 {
-	char name[1024] = "Hello_File.txt"; 
+	char source_file[1024] = "Hello_File.txt"; 
+	char dest_file[1024] = "Dest_File.txt";
 	printf("Enter the name of the source file: ");
-	printf("\nThe name of the source file is  :=  %s\n", name);
+	printf("\nThe name of the source file is       :=  %s\n", source_file);
+	printf("The name of the destination file is  :=  %s\n", dest_file);
+
 	return 0;
 }
 
