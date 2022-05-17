3d2a0e57156421b421d75de38d7ae7c9979c9fba
Juan Carlos Orlando <mrfpgapro@gmail.com>
2022-05-17 03:44:23 +0000

    May 16  -  8:44 PM

--- copy_program/main.c
+++ copy_program/main.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
-
+#include <unistd.h>
+#include <string.h>
 
 int main()
 {
 	char source_file[512] = ""; 
 	char dest_file[512] = "";
+	strcpy(source_file, "./");
 
-	printf("\nEnter the name of the source file: ");
+	printf("\nEnter the name of the source file:  ");
+	scanf("%s", dest_file);
+
+	strncat( source_file, dest_file, 128 );
+
+//	if( access( source_file, F_OK ) != -1)
+//	{
+//		printf("file is found");
+//	}
+//	else
+//  {
+//		printf("file is not found");
+//		return -1;
+//  }
+
+	strcpy(dest_file, "./");
 
 	printf("\nThe name of the source file is           :=  %s\n", source_file);
 
-	printf("\nEnter the name of the destination file: ");
-
+	printf("\nEnter the name of the destination file:  ");
+	scanf("%s", dest_file);
 	printf("\nThe name of the destination file is      :=  %s\n\n", dest_file);
 
 	return 0;
