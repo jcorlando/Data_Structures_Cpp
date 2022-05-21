b3a37a0ac52c3d4dde75ef6076ef2560f370452a
Juan Carlos Orlando <mrfpgapro@gmail.com>
2022-05-21 23:47:09 +0000

    May 21, 2022  -  4:47 PM

--- nix_kernel_module/simple.c
+++ nix_kernel_module/simple.c
@@ -2,18 +2,21 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/hash.h>
+#include <linux/gcd.h>  
 
 /* This function is called when the module is loaded. */
 static int simple_init(void)
 {
-	printk(KERN_INFO "Loading Module\n");
-	printk(KERN_INFO "%%llu∖n", GOLDEN_RATIO_PRIME);
+	printk(KERN_INFO "Loading \"simple\" Module");
+	printk(KERN_INFO "The Golden Ratio Prime is   ==   %llu\n", GOLDEN_RATIO_PRIME);
 	return 0;
 }
 
 /* This function is called when the module is removed. */
-static void simple_exit(void) {
-	printk(KERN_INFO "Removing Module\n");
+static void simple_exit(void)
+{
+	printk(KERN_INFO "Removing \"simple\" Module");
+	printk(KERN_INFO "The greatest common divisor of 3,300 and 24 is  ==  %lu\n", gcd(3300, 24));
 }
 
 /* Macros for registering module entry and exit points. */
