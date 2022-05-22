ef3ea645cd321c2d821fef7b780ef61381ba35a3
Juan Carlos Orlando <mrfpgapro@gmail.com>
2022-05-22 04:54:57 +0000

    May 21, 2022  -  9:54 PM

--- nix_proc_kernel_module_assignment_01/assignmentone.c
+++ nix_proc_kernel_module_assignment_01/assignmentone.c
@@ -3,15 +3,19 @@
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
 #include <asm/uaccess.h>
+#include <linux/jiffies.h>
 
 #define BUFFER_SIZE 128
 
-#define PROC_NAME "hello"
-#define MESSAGE "Hello World\n"
+#define PROC_NAME "jiffies"
 
-/**
- * Function prototypes
- */
+// My assignment
+// ---------------
+// Design a kernel module that creates a /proc file named /proc/jiffies that reports the
+// current value of jiffies when the /proc/jiffies file is read, such as with the command 
+// "cat /proc/jiffies"
+// Be sure to remove /proc/jiffies when the module is removed.
+
 static ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
 
 static struct proc_ops my_fops =
@@ -20,44 +24,21 @@ static struct proc_ops my_fops =
 };
 
 
-/* This function is called when the module is loaded. */
 static int proc_init(void)
 {
-	// creates the /proc/hello entry
-	// the following function call is a wrapper for
-	// proc_create_data() passing NULL as the last argument
-	
 	proc_create(PROC_NAME, 0, NULL, &my_fops);
 	printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
 
 	return 0;
 }
 
-/* This function is called when the module is removed. */
 static void proc_exit(void)
 {
-	// removes the /proc/hello entry
-
 	remove_proc_entry(PROC_NAME, NULL);
 	printk( KERN_INFO "/proc/%s removed\n", PROC_NAME);
 }
 
 
-/**
- * This function is called each time the /proc/hello is read.
- * 
- * This function is called repeatedly until it returns 0, so
- * there must be logic that ensures it ultimately returns 0
- * once it has collected the data that is to go into the 
- * corresponding /proc file.
- *
- * params:
- *
- * file:
- * buf: buffer in user space
- * count:
- * pos:
- */
 static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
 {
 	int rv = 0;
@@ -75,13 +56,11 @@ static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, 
 
 	rv = sprintf(buffer, "Hello World\n");
 	
-	// copies the contents of buffer to userspace usr_buf 
 	copyReturnValue = copy_to_user(usr_buf, buffer, rv);
 
 	return rv;
 }
 
-/* Macros for registering module entry and exit points. */
 module_init( proc_init );
 module_exit( proc_exit );
 
