82defbf93d07349254378a22b188fbcaed8c6e93
Juan Carlos Orlando <mrfpgapro@gmail.com>
2022-05-31 03:03:11 +0000

    May 30, 2022  -  8:03 PM

--- linear_algebra/chp_01_vectors_and_matrices/vect_add_sub/main.cpp
+++ linear_algebra/chp_01_vectors_and_matrices/vect_add_sub/main.cpp
@@ -3,8 +3,12 @@
 
 int main()
 {
-    Eigen::Vector3d Position(5.5, 7.3, 3.14);
-    std::cout << "\nOriginal values of Position Vector is  ==  \n" << Position << std::endl << std::endl;
+    Eigen::Vector3d position_a(5.5, 7.3, 3.14);
+    Eigen::Vector3d position_b(8.3, 1.2, 4.44);
+    std::cout << "\nOriginal values of position_a Vector are  ==  \n" << position_a << std::endl << std::endl;
+    std::cout <<   "Original values of position_b Vector are  ==  \n" << position_b << std::endl << std::endl;
+
+    std::cout << "position_a Vector + position_b Vector  ==  \n" << position_a + position_b << std::endl << std::endl;
 
     
     
