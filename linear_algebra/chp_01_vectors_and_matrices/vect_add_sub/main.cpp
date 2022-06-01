0c41b5d1eef87e7582062eb393f141b07198a835
Juan Carlos Orlando <mrfpgapro@gmail.com>
2022-06-01 01:27:39 +0000

    May 31, 2022  -  6:27 PM

--- linear_algebra/chp_01_vectors_and_matrices/vect_add_sub/main.cpp
+++ linear_algebra/chp_01_vectors_and_matrices/vect_add_sub/main.cpp
@@ -8,10 +8,25 @@ int main()
     std::cout << "\nOriginal values of position_a Vector are  ==  \n" << position_a << std::endl << std::endl;
     std::cout <<   "Original values of position_b Vector are  ==  \n" << position_b << std::endl << std::endl;
 
-    std::cout << "position_a Vector + position_b Vector  ==  \n" << position_a + position_b << std::endl << std::endl;
+    std::cout << "(position_a + position_b)  ==  \n" << position_a + position_b << std::endl << std::endl;
 
+    std::cout << "(position_a - position_b)  ==  \n" << position_a - position_b << std::endl << std::endl;
     
-    
+    std::cout << "Negation is demonstrated as such -(position_a)  ==  \n" << -position_a << std::endl << std::endl;
+
+    std::cout << "Compound operator += as in (position_a += position_b) OR (position_a = position_a + position_b)  ==  \n";
+    position_a += position_b;
+    std::cout << position_a << std::endl << std::endl;
+
+    // Reset values of vector
+    position_a(0) = 5.5;  position_a(1) = 7.3;  position_a(2) = 3.14;
+
+    std::cout << "Compound operator -= as in (position_a -= position_b) OR (position_a = position_a - position_b)  ==  \n";
+    position_a -= position_b;
+    std::cout << position_a << std::endl << std::endl;
+
+    // Reset values of vector
+    position_a(0) = 5.5;  position_a(1) = 7.3;  position_a(2) = 3.14;
 
     return 0;
 }
