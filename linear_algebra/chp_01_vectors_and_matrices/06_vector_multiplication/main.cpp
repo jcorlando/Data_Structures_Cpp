8df166ec111c6f2132696e5479e67057894f347f
Juan Carlos Orlando <mrfpgapro@gmail.com>
2022-06-05 21:00:35 +0000

    June 5, 2022  -  2:00 PM

---
+++ linear_algebra/chp_01_vectors_and_matrices/06_vector_multiplication/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+int main()
+{
+    /* THE DOT PRODUCT.
+    *  --------------------
+    *  The dot product between two n-dimensional vectors a and b is a scalar quantity.
+    *  The corresponding components of the two vectors are multiplied together and
+    *  summed. This product gets its name from the notation a · b in which a dot is placed
+    *  between the two vectors participating in the product. Because it produces a scalar
+    *  quantity, the dot product is also known as the scalar product.
+    */
+
+    // Scalar that holds the answer of the Dot Product
+    // int scalar;
+
+    // Create two vectors
+    // Eigen::Vector3i a(5, 4, 1);
+    // Eigen::Vector3i b(8, 7, 4);
+
+    // Compute Dot Product
+    // scalar = a.dot(b);
+
+    // Print results
+    // std::cout << std::endl << "The Scalar product between vector a \n\n" << a << "\n\nand vector b \n\n" << b << "\n\nis  ==   " << scalar << std::endl << std::endl;
+
+
+    /* THE CROSS PRODUCT.
+    *  -----------------------
+    *  
+    *  
+    */
+
+
+    // Create three vectors
+    Eigen::Vector3i a(5, 4, 1);
+    Eigen::Vector3i b(8, 7, 4);
+    Eigen::Vector3i answer();
+
+
+
+
+
+
+    // answer = a.cross(b);
+
+    return 0;
+}
