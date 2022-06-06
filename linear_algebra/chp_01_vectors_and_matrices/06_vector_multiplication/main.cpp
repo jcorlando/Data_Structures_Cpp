1fe169dbcbc9364407c8645351c75ddb40484bab
Juan Carlos <mrfpgapro@gmail.com>
2022-06-05 18:06:07 -0700

    June 5, 2022  -  6:06 PM

--- linear_algebra/chp_01_vectors_and_matrices/06_vector_multiplication/main.cpp
+++ linear_algebra/chp_01_vectors_and_matrices/06_vector_multiplication/main.cpp
@@ -14,37 +14,41 @@ int main()
     */
 
     // Scalar that holds the answer of the Dot Product
-    // int scalar;
+    int scalar;
 
     // Create two vectors
-    // Eigen::Vector3i a(5, 4, 1);
-    // Eigen::Vector3i b(8, 7, 4);
+    Eigen::Vector3i a(5, 4, 1);
+    Eigen::Vector3i b(8, 7, 4);
 
     // Compute Dot Product
-    // scalar = a.dot(b);
+    scalar = a.dot(b);
 
     // Print results
-    // std::cout << std::endl << "The Scalar product between vector a \n\n" << a << "\n\nand vector b \n\n" << b << "\n\nis  ==   " << scalar << std::endl << std::endl;
+    std::cout << std::endl << "The Scalar product between vector a \n\n" << a << "\n\nand vector b \n\n" << b << "\n\nis  ==   " << scalar << std::endl << std::endl;
 
 
     /* THE CROSS PRODUCT.
     *  -----------------------
-    *  
-    *  
+    *  The cross product is also known as the vector product.  The cross product produces
+    *  another vector which is orthogonal to both a and b ( a x b = c ), and the length of
+    *  the resultant vector "c" is the area of the parallelogram formed by the plane
+    *  constrained by the two vectors a and b.
     */
 
 
     // Create three vectors
-    Eigen::Vector3i a(5, 4, 1);
-    Eigen::Vector3i b(8, 7, 4);
-    Eigen::Vector3i answer();
+    Eigen::Vector3i a(1,  6, -8);
+    Eigen::Vector3i b(4, -2, -1);
+    Eigen::Vector3i c;
 
 
+    // Compute Cross Product
+    c = a.cross(b);
 
 
+    // Print results
+    std::cout << std::endl << "The Cross product between vector a \n\n" << a << "\n\nand vector b \n\n" << b << "\n\nis  ==   \n\n" << c << std::endl << std::endl;
 
-
-    // answer = a.cross(b);
 
     return 0;
 }
