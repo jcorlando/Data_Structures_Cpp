0a558d64fbe98be3d343d55d48925c9cc84b2efa
Juan Carlos Orlando <mrfpgapro@gmail.com>
2022-06-11 19:35:57 +0000

    June 11, 2022  -  12:35 PM

---
+++ operating_systems/chp_03/exercise_2/time.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/*
+*  Assignment : #2
+*  Write a C program called time.c that determines the amount of time necessary
+*  to run a command from the command line. This program will be run as "./time <command>"
+*  and will report the amount of elapsed time to run the specified command. This will involve
+*  using fork() and exec() functions, as well as the gettimeofday() function to determine
+*  the elapsed time. It will also require the use of two different IPC mechanisms.
+*/
+
+int main()
+{
+    
+    return 0;
+}
