96fb387f0b4b5434bf0c979bf160ed863c8a10b3
Juan Carlos <mrfpgapro@gmail.com>
2022-06-19 21:53:16 -0700

    June 19, 2022  -  9:53 PM

--- operating_systems/chp_03/exercise_3/my_pid_manager.c
+++ operating_systems/chp_03/exercise_3/my_pid_manager.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Constants to identify the range of
-// possible pid values:
+
+// Macros to identify the range
+// of possible pid values:
 #define MIN_PID 300
 #define MAX_PID 5000
+#define BITS (MAX_PID - MIN_PID)
+
+// Macros for defining Active and In-Active processes
+#define ACTIVE 1
+#define INACTIVE 0
 
 
-#define BITS 4
-static struct PID_BITFIELD
+// Bit-field struct
+struct PID_BITFIELD
 {
-    unsigned bit : 1;
-} active_pids[BITS];
+    u_int8_t bit : 1; //<--- 1 bit in size
+};
+
+// Create Static Bit-Field Instance
+static struct PID_BITFIELD pids_bits[BITS];
+
+
 
 // Function protoypes
 int allocate_map();
@@ -19,23 +30,79 @@ int allocate_pid();
 void release_pid(int pid);
 
 
+// My header file for manipulating the bit field.
+// Functions include "Set-bit", "Check-bit"... etc.
+#include "my_pid_manager.h"
+
 int main()
 {
-    active_pids[0].bit = 0;
-    active_pids[1].bit = 1;
-    // implicit truncation from 'int' to bit-field changes value from 2 to 0 [-Wbit/field-constant-conversion]
-    // Above comment warnings occur if you change below right side equals values to 2 and 3 respectively
-    active_pids[2].bit = 2;
-    active_pids[3].bit = 3;
-
-    int i;
-    for ( i=0; i < BITS ; i++)
+    if ( allocate_map() == -1 )
     {
-        printf("bit of bit %i is %u\n", i, active_pids[i].bit);
+        printf("Error creating PID Bit-field");
+        return EXIT_FAILURE;
     }
 
-    printf( "\nSize of bit is  ==>  %lu\n\n", sizeof(active_pids) );
 
 
+    // implicit truncation from 'int' to bit-field changes value from 2 to 0 [-Wbit/field-constant-conversion]
+    // Above comment warnings occur if you change below right side equals values to 2 and 3 respectively
+    // Use flag -Werror to convert warnings into compile time errors.
+
+
+
+    
+    // Print out entire bit-field
+    for (uint index = 0; index < BITS; index++)
+    {
+        if (index % 55 == 0) printf("\n");
+
+        // ----------------------Print Backwards
+        printf( "%u ", pids_bits[BITS - index - 1].bit );
+    }
+
+    printf("\n\n");
+
+
+
+
+
+
+
+    
     return EXIT_SUCCESS;
 }
+
+
+// Create Data-Structure that will hold
+// Process ID's and their availability
+int allocate_map()
+{
+    if (pids_bits == NULL)
+        return -1;
+    
+
+    for (uint i = 0; i < MAX_PID - MIN_PID + 1; i++)
+    {
+        pids_bits[i].bit = INACTIVE;
+    }
+
+    return 1;
+}
+
+
+// Check to see if there's an available process ID,
+// then assign the process an ID if there is one available.
+int allocate_pid()
+{
+    for (uint i = 0; i < BITS; i++)
+    {
+        if (1)
+        {
+            // pId[i].isAvailable = FALSE;
+            return MIN_PID + i;
+        }
+    }
+
+    return -1;
+}
+
