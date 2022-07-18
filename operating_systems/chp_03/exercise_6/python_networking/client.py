7bff266a12368f2e7a136ff695f77a9454d252bf
Juan Carlos <mrfpgapro@gmail.com>
2022-07-17 17:19:18 -0700

    July 17, 2022  -  5:19 PM

--- operating_systems/chp_03/exercise_6/python_networking/client.py
+++ operating_systems/chp_03/exercise_6/python_networking/client.py
@@ -1,13 +1,13 @@
 import socket
 
+#------ Broadcast address, (0.0.0.0) is used for when running on cloud server
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as my_socket:
     my_socket.connect( (HOST, PORT) )
-    my_socket.sendall( b"Hello, world" )
     data = my_socket.recv(1024)
 
-print( f"Received {data!r}" )
+print( data.decode() )
 
 
--- operating_systems/chp_03/exercise_6/python_networking/server.py
+++ operating_systems/chp_03/exercise_6/python_networking/server.py
@@ -1,19 +1,38 @@
 import socket
+import threading
 
-HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
+# This example creates threads and continuously runs after each connection
+# and prints the message of the day until you use Kill (PID) or CTRL-C to
+# kill the process.  My suggestion is to run the program from a terminal using
+# the command "python server.py &" so that it runs in the background.
+
+#------ Broadcast address, (0.0.0.0) is used for when running on cloud server
+HOST = "127.0.0.1" #<--- Standard loopback interface address (localhost) (127.0.0.1)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as my_socket:
     my_socket.bind( (HOST, PORT) )
-    my_socket.listen()
-    ( conn, addr ) = my_socket.accept() #<---This part is blocking
-    with conn:
-        print( f"Connected by {addr}" )
+
+
+    def handle_client(conn, addr):
+        print(f"[NEW CONNECTION] {addr} connected")
+        with conn:
+            print( f"Connected by {addr}" )
+            mesg_day = "Message of the Day!!"
+            conn.sendall( mesg_day.encode() )
+
+    def start():
+        my_socket.listen()
         while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+            ( conn, addr ) = my_socket.accept() #<---This part is blocking
+            thread = threading.Thread( target=handle_client, args=(conn, addr) )
+            thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
 
+    print( "[STARTING] server is starting" )
+    start()
 
+    
+    
