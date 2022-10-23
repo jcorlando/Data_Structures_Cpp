5750e4ab3ac109f0a5ced606ebe533592c782b73
Jabberwocky <mrfpgapro@gmail.com>
2022-10-22 17:45:52 -0700

    Continued Development on main PC.

--- invert_tree_structure/invert_tree.cpp
+++ invert_tree_structure/invert_tree.cpp
@@ -2,13 +2,21 @@
 #include <vector>
 
 
+
+
+
 int main()
 {
     int myVariable = 23;
 
     std::cout << myVariable << std::endl;
 
-    
+
+
+
+    // Always add this to the end of every program to
+    // signify to the terminal the program has ended
+    std::cout << "\n\n\n" << std::endl;
+
     return 0;
 }
-
