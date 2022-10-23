5df67ab9808c2dff5aabbfdcc11826a78e47c823
Jabberwocky <mrfpgapro@gmail.com>
2022-10-22 19:40:41 -0700

    Optimized VS Code Build and Dev Environment.

--- invert_tree_structure/invert_tree.cpp
+++ invert_tree_structure/invert_tree.cpp
@@ -2,21 +2,18 @@
 #include <vector>
 
 
-
+struct item
+{
+  std::string itemName;
+};
 
 
 int main()
 {
-    int myVariable = 23;
 
-    std::cout << myVariable << std::endl;
-
-
-
-
-    // Always add this to the end of every program to
+    // Always append this to the end of every program to
     // signify to the terminal the program has ended
-    std::cout << "\n\n\n" << std::endl;
+    std::cout << "\n\n\nProgram terminated successfully\n\n" << std::endl;
 
     return 0;
 }
